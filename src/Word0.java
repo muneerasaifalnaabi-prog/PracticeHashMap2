7228eec670c9c737e4c3c332ae91abeb321992b4
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 20:48:51 +0400

    added class word0

--- src/Word0.java
+++ src/Word0.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Word0 {
     public static void main(String[] args) {
+
+    }
+    public static Map<String,Integer> Word0(String[] words){
+        Map<String,Integer> map = new HashMap<>();
 
     }
 }
