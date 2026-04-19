ee2008b8aec3ff8efc1e4a8930030489e0b2b8a8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 21:07:19 +0400

    Added class wordLen

--- src/WordLen.java
+++ src/WordLen.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordLen {
     public static void main(String[] args) {
 
     }
-    public static  Map<String, Integer> wordLen
+    public static Map<String, Integer> wordLen(String []arr){
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : arr) {
+            map.put(word,word.length());
+
+        }
+
+    }
 
 
 }
