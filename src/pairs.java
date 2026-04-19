79ac306e2a4b2c753f34cba91864bd5d16a8ad1f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-19 21:15:51 +0400

    Added class pairs

--- src/pairs.java
+++ src/pairs.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class pairs {
     public static void main(String[] args) {
+
+    }
+    public static Map<String, String> wordLen(String []arr){
+        Map<String, String> map = new HashMap<>();
+
+
 
     }
 }
