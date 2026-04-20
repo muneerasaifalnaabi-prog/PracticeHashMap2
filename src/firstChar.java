d5a0a51bd0f0c9cba4ac685eb4f9e901c0e27f34
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 11:04:13 +0400

    added class firstChar

--- src/firstChar.java
+++ src/firstChar.java
@@ -1,5 +1,10 @@
+import java.util.Map;
+
 public class firstChar {
     public static void main(String[] args) {
+
+    }
+    public static Map<String, Integer> wordCount(String [] arr ){
 
     }
 
