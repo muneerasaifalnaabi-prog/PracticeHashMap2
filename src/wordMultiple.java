0f2ee3836d22aad0c3847f1e96c526238d193b59
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 11:49:14 +0400

    Added class wordMultiple

--- src/wordMultiple.java
+++ src/wordMultiple.java
@@ -1,3 +1,11 @@
-public class wordMultiple {
+import java.util.Map;
 
+public class wordMultiple {
+    public static void main(String[] args) {
+
+    }
+    public static Map<String, Boolean> firstChar(String [] arr ){
+
+
+    }
 }
