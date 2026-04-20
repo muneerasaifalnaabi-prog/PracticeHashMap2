b41b46c1f5c6d962c7a447240c32326d6f3a6156
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 11:21:57 +0400

    added class wordAppend

--- src/wordAppend.java
+++ src/wordAppend.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class wordAppend {
     public static void main(String[] args) {
 
     }
 
+    public static String wordAppend(String[] arr) {
+        Map<String, String> map = new HashMap<>();
+
+        for (String s:arr){
+            if
+        }
+
+    }
 }
