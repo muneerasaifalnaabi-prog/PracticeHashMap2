526d818e22315c81c5cba746d409ad6549fea85f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 12:19:02 +0400

    Added class allSwap

--- src/allSwap.java
+++ src/allSwap.java
@@ -5,11 +5,10 @@ public class allSwap {
     public static void main(String[] args) {
 
     }
-    public static Map<String, String> wordMultiple(String [] arr ) {
+    public static String[]  wordMultiple(String [] arr ) {
         Map<String, String> map = new HashMap<>();
-        for (String s:arr){
-            String firstChar=String.valueOf(s.charAt(0));
-           String swap="";
+
+        for (int i=0;i<arr.length;i++){
 
         }
     }
