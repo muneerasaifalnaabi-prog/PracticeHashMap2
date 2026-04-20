564d0eff913caab20fa5bea8d3acf0a75f53abd1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 10:42:04 +0400

    updated class wordCount

--- src/wordCount.java
+++ src/wordCount.java
@@ -1,5 +1,14 @@
+import java.util.Map;
+
 public class wordCount {
     public static void main(String[] args) {
+
+    }
+    public static Map<String, Integer> pairs(String []arr){
+        int count =0;
+        for (int i =0;i< arr.length;i++){
+            if (arr[i]==i)
+        }
 
     }
 }
