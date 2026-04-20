72c0596a49f0b91762fcc29fcdfc3af353f68c7f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 13:49:42 +0400

    Completed Task9 firstSwap

--- src/firstSwap.java
+++ src/firstSwap.java
@@ -1,6 +1,34 @@
+import java.util.HashMap;
+
 public class firstSwap {
     public static void main(String[] args) {
+        String[] words = {"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"};
+        String[] result = firstSwap(words);
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+    public static  String[] firstSwap(String[] arr) {
+        HashMap<String,Integer> map = new HashMap<>();
+        int counter = 0;
+        int index = 0;
+        for (String s : arr){
+            String firstChar = s.substring(0,1);
+            if(map.containsKey(firstChar)){
+                if(map.get(firstChar) > -1){
+                    index = map.get(firstChar);
+                    String temp = arr[counter];
+                    arr[counter] = arr[index];
+                    arr[index] = temp;
+                    map.put(firstChar,-1);
+                }
+            } else {
+                map.put(firstChar,counter);
+            }
 
+            counter = counter + 1;
+        }
+        return arr;
     }
 
 }
