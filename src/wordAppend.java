240d8ba5b007475310daa1152d95eba5d0bcdfa9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-20 11:42:16 +0400

    Completed Task6 wordAppend

--- src/wordAppend.java
+++ src/wordAppend.java
@@ -3,18 +3,27 @@ import java.util.Map;
 
 public class wordAppend {
     public static void main(String[] args) {
-
+        String[] words = {"a", "b", "a"};
+        String[] words1 = {"a", "b", "a", "c", "a", "d", "a"};
+        System.out.println(wordAppend(words));
+        System.out.println(wordAppend(words1));
     }
 
     public static String wordAppend(String[] arr) {
-        Map<String, String> map = new HashMap<>();
-        String
-        for (String s :arr){
-        String key =String.valueOf(s.charAt(0));
-        if (map.containsKey(key)) {
+        Map<String, Integer> map = new HashMap<>();
+        String result ="";
+        for (String s :arr) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+            if (map.get(s) % 2 == 0) {
+                result+=s;
+            }
+
 
         }
-        }
-
+        return result;
     }
 }
